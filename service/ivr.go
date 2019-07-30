8a3997bbe77133ca550a2244cb5bad7c6661f849
yy <1023767856@qq.com>
2019-07-30 18:39:43 +0800

    change url methods

--- service/ivr.go
+++ service/ivr.go
@@ -19,7 +19,7 @@ func Hook(ctx *gin.Context) {
 	cmd.Args = args
 	err := cmd.Start()
 	if err != nil {
-		utils.Success(ctx, "shell execute failed")
+		utils.Success(ctx, err)
 		return
 	}
 	utils.Success(ctx, shellPath)
